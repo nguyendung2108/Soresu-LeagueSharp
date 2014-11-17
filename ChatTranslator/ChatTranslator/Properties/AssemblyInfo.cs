fbc49a01e4b6392a821b12023c0036af2a0c4918
Soresu <g_mester@freemail.hu>
2014-11-17 16:35:32 +0100

    Update AssemblyInfo.cs

--- ChatTranslator/ChatTranslator/Properties/AssemblyInfo.cs
+++ ChatTranslator/ChatTranslator/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ChatTranslator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Soresu")]
 [assembly: AssemblyProduct("ChatTranslator")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
