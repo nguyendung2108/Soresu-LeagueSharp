167436b5334576ec347f806379455b024e2cac6a
Soresu <g_mester@freemail.hu>
2014-12-29 20:19:47 +0100

    Upload

---
+++ TeamStats/TeamStats/Program.cs
@@ -0,0 +1,110 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.Direct3D9;
+using TeamStats.Properties;
+using Color = System.Drawing.Color;
+namespace TeamStats
+{
+    class Program
+    {
+        public static Menu Config;
+        public static readonly Obj_AI_Hero player = ObjectManager.Player;
+        public static Teams teams;
+        private static Render.Sprite frame;
+        public static int myTeamHpX = (int)(Drawing.Width*0.68);
+        public static int myTeamHpY = (int)(Drawing.Height * 0.97);
+        public static int enemyTeamHpX = (int)(Drawing.Width * 0.68);
+        public static int enemyTeamHpY = (int)(Drawing.Height * 0.97) - 40;
+        public static int myTeamDmgX = (int)(Drawing.Width * 0.68);
+        public static int myTeamDmgY = (int)(Drawing.Height * 0.97) - 20;
+        public static int enemyTeamDmgX = (int)(Drawing.Width * 0.68);
+        public static int enemyTeamDmgY = (int)(Drawing.Height * 0.97) - 60;
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Config = new Menu("TeamStats", "TeamStats", true);
+            Config.AddItem(new MenuItem("X-pos", "X-pos").SetValue(new Slider(0, -1500, 400)));
+            Config.AddItem(new MenuItem("Y-pos", "Y-pos").SetValue(new Slider(0, 200, -1080)));
+            Config.AddItem(new MenuItem("Default", "Default").SetValue(false));
+            Config.AddItem(new MenuItem("Enabled", "Enabled").SetValue(true));
+            Config.AddToMainMenu();
+            //frame = loadFrame();
+            Drawing.OnDraw += Drawing_OnDraw;
+            Game.OnGameUpdate += Game_OnGameUpdate;
+            Game.PrintChat("<font color='#9933FF'>Soresu </font><font color='#FFFFFF'>- TeamStats</font>");
+        }
+
+        private static void Game_OnGameUpdate(EventArgs args)
+        {
+            if (Config.Item("Enabled").GetValue<bool>() && player.CountEnemysInRange(2200) > 0) teams = new Teams();
+            if (Config.Item("Default").GetValue<bool>())
+            {
+                Config.Item("Y-pos").SetValue(new Slider(0, 200, -1080));
+                Config.Item("X-pos").SetValue(new Slider(0, -1500, 400));
+                Config.Item("Default").SetValue(false);
+            }
+            
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if (Config.Item("Enabled").GetValue<bool>() && player.CountEnemysInRange(2200) > 0)
+            {
+
+                var OffsetX = Config.Item("X-pos").GetValue<Slider>().Value;
+                var OffsetY = Config.Item("Y-pos").GetValue<Slider>().Value;
+                int myteamhpBar = teams.myTeamHP;
+                int enemyteamhpBar = teams.enemyTeamHP;
+                int myteamdmgBar = teams.myTeamDmg;
+                int enemyteamdmgBar = teams.enemyTeamDmg;
+                int highest1 = Math.Max(myteamhpBar, enemyteamhpBar);
+                int highest2 = Math.Max(myteamdmgBar, enemyteamdmgBar);
+                int highest = Math.Max(highest1, highest2);
+                float scale = 300f / (highest+500);
+
+                Drawing.DrawLine(myTeamHpX + OffsetX, myTeamHpY+14 + OffsetY, enemyTeamDmgX + OffsetX, enemyTeamDmgY + OffsetY, 300, Color.SlateGray);
+                Drawing.DrawText(myTeamHpX + OffsetX, myTeamHpY + OffsetY - 2, Color.White, "Allies Health");
+                Drawing.DrawLine(myTeamHpX + OffsetX, myTeamHpY + OffsetY, myTeamHpX + OffsetX, myTeamHpY + OffsetY + 14, (int)(myteamhpBar * scale * -1), Color.ForestGreen);
+
+                Drawing.DrawText(enemyTeamHpX + OffsetX, enemyTeamHpY + OffsetY - 2, Color.White, "Enemies Healt");
+                Drawing.DrawLine(enemyTeamHpX + OffsetX, enemyTeamHpY + OffsetY, enemyTeamHpX + OffsetX, enemyTeamHpY + OffsetY + 14, (int)(-enemyteamhpBar * scale), Color.ForestGreen);
+
+                Drawing.DrawText(myTeamDmgX + OffsetX, myTeamDmgY + OffsetY - 2, Color.White, "Allies Damage");
+                Drawing.DrawLine(myTeamDmgX + OffsetX, myTeamDmgY + OffsetY, myTeamDmgX + OffsetX, myTeamDmgY + OffsetY + 14, (int)(-myteamdmgBar * scale), Color.Firebrick);
+
+                Drawing.DrawText(enemyTeamDmgX + OffsetX, enemyTeamDmgY + OffsetY - 2, Color.White, "Enemies Damage");
+                Drawing.DrawLine(enemyTeamDmgX + OffsetX, enemyTeamDmgY + OffsetY, enemyTeamDmgX + OffsetX, enemyTeamDmgY + OffsetY + 14, (int)(-enemyteamdmgBar * scale), Color.Firebrick);
+                if (myteamhpBar-enemyteamdmgBar<enemyteamhpBar-myteamdmgBar)
+                {
+                    Drawing.DrawText(myTeamHpX+ 60 + OffsetX, myTeamHpY + OffsetY +14, Color.Red, "Enemy team is stronger");
+                }
+                else
+                {
+                    Drawing.DrawText(myTeamHpX + 55 + OffsetX, myTeamHpY + OffsetY +14, Color.ForestGreen, "Your team is stronger");
+                }
+                
+            }
+        }
+        private static Render.Sprite loadFrame()
+        {
+
+                var load = new Render.Sprite(Resources.tsframe, new SharpDX.Vector2(0, 0))
+                {
+                    Color = new SharpDX.ColorBGRA(255f, 255f, 255f, 20f)
+                };
+                load.Position = new SharpDX.Vector2((int)(Drawing.Width * 0.628), (int)(Drawing.Height - 102));
+                load.Show();
+                load.Add(0);
+                return load;
+        }
+    }
+}
---
+++ TeamStats/TeamStats/Teams.cs
@@ -0,0 +1,110 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamStats
+{
+    internal class Teams
+    {
+        public List<Obj_AI_Hero> myTeam = new List<Obj_AI_Hero>();
+        public List<Obj_AI_Hero> EnemyTeam = new List<Obj_AI_Hero>();
+        public int myTeamNum;
+        public int enemyTeamNum;
+        public int myTeamDmg;
+        public int enemyTeamDmg;
+        public int myTeamHP { get; set; }
+        public int enemyTeamHP { get; set; }
+
+        public Teams()
+        {
+            SetNums();
+        }
+        public void SetNums()
+        {
+            foreach (var player in ObjectManager.Get<Obj_AI_Hero>().Where(i => !i.IsDead && !i.IsMinion && Program.player.Distance(i) < 2200))
+            {
+                //list
+                if (player.IsEnemy) EnemyTeam.Add(player);
+                if (player.IsAlly || player.IsMe) myTeam.Add(player);
+                //num
+                if (player.IsEnemy) enemyTeamNum++;
+                if (player.IsAlly || player.IsMe) myTeamNum++;
+            }
+            myTeam = myTeam.OrderByDescending(i => i.ChampionsKilled).ToList();
+            EnemyTeam = EnemyTeam.OrderByDescending(i => i.ChampionsKilled).ToList();
+            var e = 0;
+            var eHP = EnemyTeam[0].Health;
+            foreach (var enemy in EnemyTeam)
+            {
+                if (eHP < 0)
+                {
+                    e++;
+                    eHP = myTeam[e].Health;
+                }
+                enemyTeamDmg += (int)ComboDamage(enemy, myTeam[e]);
+                eHP -=(int)enemy.GetComboDamage(myTeam[e], new[] {SpellSlot.Q, SpellSlot.W, SpellSlot.E, SpellSlot.R});
+                enemyTeamHP += (int)((enemy.Health * ((1 + (1 - (100 / (enemy.Armor + 100))))) + enemy.Health * (0.3 + 1)) / 2);
+
+            }
+            var t = 0;
+            var tHP = EnemyTeam[0].Health;
+            foreach (var teammate in myTeam)
+            {
+                if (tHP < 0)
+                {
+                    t++;
+                    tHP = EnemyTeam[t].Health;
+                }
+                myTeamDmg += (int)ComboDamage(teammate, EnemyTeam[t]);
+                tHP -= (int)teammate.GetComboDamage(EnemyTeam[t], new[] { SpellSlot.Q, SpellSlot.W, SpellSlot.E, SpellSlot.R });
+                myTeamHP += (int)((teammate.Health * ((1 + (1 - (100 / (teammate.Armor + 100))))) + teammate.Health * (0.3 + 1)) / 2);
+            }
+        }
+        private static float ComboDamage(Obj_AI_Hero src, Obj_AI_Hero dsc)
+        {
+            Random rnd = new Random();
+            float basicDmg = 0;
+            int attacks = (int)Math.Floor(src.AttackSpeedMod*5);
+            for (int i = 0; i < attacks; i++)
+            {
+                if (src.Crit>0)
+                {
+                    basicDmg += (src.BaseAttackDamage + src.FlatPhysicalDamageMod)*(1 + src.Crit);
+                }
+                else
+                {
+                    basicDmg += src.BaseAttackDamage + src.FlatPhysicalDamageMod;
+                }  
+            };
+            float damage = basicDmg;
+            var spells = src.Spellbook.Spells;
+            foreach (var spell in spells)
+            {
+                var t = spell.CooldownExpires - Game.Time;
+                
+                if (t < 0.5 && spell.Level > 0 && spell.SData.SpellCastTime < 2f)
+                {
+                    if (spell.Cooldown < 2.5f && spell.Cooldown>0)
+                    {
+                        int count = (int)Math.Floor(5/spell.Cooldown);
+                        for (int i = 0; i < count; i++)
+                        {
+                            damage += (float)Damage.GetSpellDamage(src, dsc, spell.Slot); 
+                        }
+                        
+                    }
+                    
+                }
+            }
+            if (src.Spellbook.CanUseSpell(src.GetSpellSlot("summonerdot")) == SpellState.Ready)
+            {
+                damage += (float)src.GetSummonerSpellDamage(dsc, Damage.SummonerSpell.Ignite);
+            }
+            return damage;
+        }
+     }
+ }
