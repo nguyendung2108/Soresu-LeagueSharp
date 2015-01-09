d9f2bc77bdebaa54572961a805d2fb008dad35e8
Soresu <g_mester@freemail.hu>
2015-01-09 12:54:44 +0100

    Upload

---
+++ Soresu - ChoGath/Soresu - ChoGath/Others/CF.cs
@@ -0,0 +1,242 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+using System.Linq;
+using SharpDX;
+using System.Drawing;
+using Color = System.Drawing.Color;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Soresu___ChoGath;
+
+namespace Others
+{
+
+    class CF
+    {
+        public static Items.Item botrk = new Items.Item(3153, 450);
+        public static Items.Item bilgewater = new Items.Item(3144, 450);
+        public static Items.Item hexgun = new Items.Item(3146, 700);
+        public static Items.Item Dfg = new Items.Item(3128, 750);
+        public static Items.Item Bft = new Items.Item(3188, 750);
+        public static readonly Obj_AI_Hero player = Program.player;
+        public static Spell Q=Program.Q, W=Program.W, E=Program.E, R=Program.R, RFlash=Program.RFlash;
+        private static readonly string[] jungleMonsters = { "TT_Spiderboss", "SRU_Blue", "SRU_Red", "SRU_Dragon", "SRU_Baron" };
+        private static List<string> dotsHighDmg = new List<string>(new string[] { "karthusfallenonecastsound", "CaitlynAceintheHole", "zedulttargetmark", "timebombenemybuff", "VladimirHemoplague" });
+        private static List<string> dotsMedDmg = new List<string>(new string[] { "summonerdot", "cassiopeiamiasmapoison", "cassiopeianoxiousblastpoison", "bantamtraptarget", "explosiveshotdebuff", "swainbeamdamage", "SwainTorment", "AlZaharMaleficVisions", "fizzmarinerdoombomb" });
+        private static List<string> dotsSmallDmg = new List<string>(new string[] { "deadlyvenom", "toxicshotparticle", "MordekaiserChildrenOfTheGrave", "DariusNoxianTacticsONH" });
+        public static void UseSpells(Obj_AI_Hero target)
+        {
+            if (player.Distance(target) < 400)
+            {
+                if (Items.HasItem(3077) && Items.CanUseItem(3077))
+                    Items.UseItem(3077);
+                if (Items.HasItem(3074) && Items.CanUseItem(3074))
+                    Items.UseItem(3074);
+            }
+            if (Items.HasItem(3144) && Items.CanUseItem(3144))
+            {
+                bilgewater.Cast(target);
+            }
+            if (Items.HasItem(3153) && Items.CanUseItem(3153) && player.Health < player.MaxHealth / 2)
+            {
+                botrk.Cast(target);
+            }
+            if (Items.HasItem(3146) && Items.CanUseItem(3146))
+            {
+                hexgun.Cast(target);
+            }
+            if (Items.HasItem(Dfg.Id) && Items.CanUseItem(Dfg.Id))
+            {
+                Dfg.Cast(target);
+            }
+            if (Items.HasItem(Bft.Id) && Items.CanUseItem(Bft.Id))
+            {
+                Bft.Cast(target);
+            }
+            if (player.Spellbook.CanUseSpell(player.GetSpellSlot("summonerdot")) == SpellState.Ready)
+            {
+                if (player.Distance(target) < 650 && ComboDamage(target) >= target.Health && (float)player.GetSummonerSpellDamage(target, Damage.SummonerSpell.Ignite) >= target.Health)
+                {
+                    player.Spellbook.CastSpell(player.GetSpellSlot("SummonerDot"), target);
+                }
+            }
+        }
+
+        public static float ComboDamage(Obj_AI_Hero hero)
+        {
+            float damage = 0;
+            if (Q.IsReady()) damage += (float)Damage.GetSpellDamage(player, hero, SpellSlot.Q);
+            if (E.IsReady()) damage += (float)Damage.GetSpellDamage(player, hero, SpellSlot.E);
+            if (W.IsReady()) damage += (float)Damage.GetSpellDamage(player, hero, SpellSlot.W);
+            if ((Items.HasItem(Bft.Id) && Items.CanUseItem(Bft.Id)) ||
+                (Items.HasItem(Dfg.Id) && Items.CanUseItem(Dfg.Id)))
+                damage = (float)(damage * 1.2);
+            if (Items.HasItem(Bft.Id) && Items.CanUseItem(Bft.Id))
+            {
+                damage += (float)player.GetItemDamage(hero, Damage.DamageItems.Dfg);
+            }
+            if (Items.HasItem(Dfg.Id) && Items.CanUseItem(Dfg.Id))
+            {
+                damage += (float)player.GetItemDamage(hero, Damage.DamageItems.Dfg);
+            }
+
+            if (R.IsReady()) damage += (float)Damage.GetSpellDamage(player, hero, SpellSlot.R);
+
+            if (player.Spellbook.CanUseSpell(player.GetSpellSlot("summonerdot")) == SpellState.Ready && hero.Health < damage + player.GetSummonerSpellDamage(hero, Damage.SummonerSpell.Ignite))
+            {
+                damage += (float)player.GetSummonerSpellDamage(hero, Damage.SummonerSpell.Ignite);
+            }
+            if (Items.HasItem(3077) && Items.CanUseItem(3077))
+            {
+                damage += (float)player.GetItemDamage(hero, Damage.DamageItems.Tiamat);
+            }
+            if (Items.HasItem(3074) && Items.CanUseItem(3074))
+            {
+                damage += (float)player.GetItemDamage(hero, Damage.DamageItems.Hydra);
+            }
+            if (Items.HasItem(3144) && Items.CanUseItem(3144))
+            {
+                damage += (float)player.GetItemDamage(hero, Damage.DamageItems.Bilgewater);
+            }
+            if (Items.HasItem(3153) && Items.CanUseItem(3153))
+            {
+                damage += (float)player.GetItemDamage(hero, Damage.DamageItems.Botrk);
+            }
+            if (Items.HasItem(3146) && Items.CanUseItem(3146))
+            {
+                damage += (float)player.GetItemDamage(hero, Damage.DamageItems.Hexgun);
+            }
+            if (Items.HasItem(3091) && Items.CanUseItem(3091))
+            {
+                damage += (float)player.CalcDamage(hero, Damage.DamageType.Magical, 42);
+            }
+            return damage;
+        }
+        public static Obj_AI_Minion GetNearest(Vector3 pos)
+        {
+            var minions =
+            ObjectManager.Get<Obj_AI_Minion>()
+            .Where(minion => minion.IsValid && jungleMonsters.Any(name => minion.Name.StartsWith(name)) && !jungleMonsters.Any(name => minion.Name.Contains("Mini")) && !jungleMonsters.Any(name => minion.Name.Contains("Spawn")));
+            var objAiMinions = minions as Obj_AI_Minion[] ?? minions.ToArray();
+            Obj_AI_Minion sMinion = objAiMinions.FirstOrDefault();
+            double? nearest = null;
+            foreach (Obj_AI_Minion minion in objAiMinions)
+            {
+                double distance = Vector3.Distance(pos, minion.Position);
+                if (nearest == null || nearest > distance)
+                {
+                    nearest = distance;
+                    sMinion = minion;
+                }
+            }
+            return sMinion;
+        }
+
+        public static void DrawCircle(string menuItem, float spellRange)
+        {
+            Circle circle = Program.config.Item(menuItem).GetValue<Circle>();
+            if (circle.Active) Utility.DrawCircle(player.Position, spellRange, circle.Color);
+        }
+
+        public static double smiteDamage()
+        {
+            int level = ObjectManager.Player.Level;
+            int[] damage =
+                {
+                20*level + 370,
+                30*level + 330,
+                40*level + 240,
+                50*level + 100
+                };
+            return damage.Max();
+        }
+        //Kurisu
+        public static readonly int[] SmitePurple = { 3713, 3726, 3725, 3726, 3723 };
+        public static readonly int[] SmiteGrey = { 3711, 3722, 3721, 3720, 3719 };
+        public static readonly int[] SmiteRed = { 3715, 3718, 3717, 3716, 3714 };
+        public static readonly int[] SmiteBlue = { 3706, 3710, 3709, 3708, 3707 };
+        public static string smitetype()
+        {
+            if (SmiteBlue.Any(id => Items.HasItem(id)))
+            {
+                return "s5_summonersmiteplayerganker";
+            }
+            if (SmiteRed.Any(id => Items.HasItem(id)))
+            {
+                return "s5_summonersmiteduel";
+            }
+            if (SmiteGrey.Any(id => Items.HasItem(id)))
+            {
+                return "s5_summonersmitequick";
+            }
+            if (SmitePurple.Any(id => Items.HasItem(id)))
+            {
+                return "itemsmiteaoe";
+            }
+            return "summonersmite";
+        }
+
+        public static void setSmiteSlot()
+        {
+            foreach (var spell in ObjectManager.Player.Spellbook.Spells.Where(spell => String.Equals(spell.Name, smitetype(), StringComparison.CurrentCultureIgnoreCase)))
+            {
+                Program.smiteSlot = spell.Slot;
+                Program.smite = new Spell(Program.smiteSlot, 700);
+                return;
+            }
+        }
+        public static void CastSmite(Obj_AI_Minion target)
+        {
+            Program.smite.Slot = Program.smiteSlot;
+            ObjectManager.Player.Spellbook.CastSpell(Program.smiteSlot, target);
+        }
+
+        public static int countMinionsInrange(Obj_AI_Minion l, float p)
+        {
+            return ObjectManager.Get<Obj_AI_Minion>().Count(i => !i.IsDead && i.IsEnemy && l.Distance(i) < p);
+
+        }
+        public static int countTurretsInRange(Obj_AI_Hero l)
+        {
+            return ObjectManager.Get<Obj_AI_Turret>().Count(i => !i.IsDead && i.IsEnemy && l.Distance(i) < 750f);
+
+        }
+        public static bool CheckCriticalBuffs(Obj_AI_Hero i)
+        {
+            foreach (BuffInstance buff in i.Buffs)
+            {
+                if (i.Health <= 30 && dotsSmallDmg.Contains(buff.Name))
+                {
+                    return true;
+                }
+                if (i.Health <= 60 && dotsMedDmg.Contains(buff.Name))
+                {
+                    return true;
+                }
+                if (i.Health <= 150 && dotsHighDmg.Contains(buff.Name))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Soresu - ChoGath/Soresu - ChoGath/Program.cs
@@ -0,0 +1,355 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+using System.Linq;
+using SharpDX;
+using System.Drawing;
+using Color = System.Drawing.Color;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Others;
+using SharpDX.Direct3D9;
+
+namespace Soresu___ChoGath
+{
+    class Program
+    {
+        public static Menu config;
+        private static Orbwalking.Orbwalker orbwalker;
+        public static readonly Obj_AI_Hero player = ObjectManager.Player;
+        public static Spell Q, W, E, R, RFlash, smite;
+        public static SpellSlot smiteSlot = SpellSlot.Unknown;
+        public static List<int> silence = new List<int>(new int[] { 1500, 1750, 2000, 2250, 2500});
+        public static int knockUp = 1000;
+        public static bool flashRblock = false; 
+        public static bool vSpikes=false;
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            if (player.BaseSkinName != "Chogath") return;
+            InitMenu();
+            InitChoGath();
+            Game.PrintChat("<font color='#9933FF'>Soresu </font><font color='#FFFFFF'>- Cho'Gath</font>");
+            Game.OnGameUpdate += Game_OnGameUpdate;
+            Drawing.OnDraw += Game_OnDraw;
+            AntiGapcloser.OnEnemyGapcloser += OnEnemyGapcloser;
+            Interrupter.OnPossibleToInterrupt += OnPossibleToInterrupt;
+        }
+        private static void Game_OnGameUpdate(EventArgs args)
+        {
+            vSpikes = false;
+            foreach (var buff in player.Buffs)
+            {
+                if (buff.Name == "VorpalSpikes") vSpikes = true;
+            }
+            if (CF.countTurretsInRange(player) > 0 && vSpikes && E.GetHitCount() > 0)
+            {
+                E.Cast();
+            }
+            bool minionBlock = false;
+            foreach (Obj_AI_Minion minion in MinionManager.GetMinions(player.Position, player.AttackRange, MinionTypes.All, MinionTeam.Enemy, MinionOrderTypes.None))
+            {
+                if (HealthPrediction.GetHealthPrediction(minion, 3000) <= Damage.GetAutoAttackDamage(player, minion, false))
+                    minionBlock = true;
+            }
+            if (config.Item("useRJ").GetValue<bool>() || config.Item("useSmite").GetValue<bool>())
+            {
+                Jungle();
+            }
+            switch (orbwalker.ActiveMode)
+            {
+                case Orbwalking.OrbwalkingMode.Combo:
+                    Combo();
+                    break;
+                case Orbwalking.OrbwalkingMode.Mixed:
+                    if (!minionBlock)
+                    {
+                        Harass();
+                    }
+
+                    break;
+                case Orbwalking.OrbwalkingMode.LaneClear:
+                    Clear();
+                    break;
+                case Orbwalking.OrbwalkingMode.LastHit:
+                    break;
+                default:
+                    if (!minionBlock)
+                    {
+                    }
+                    break;
+            }
+        }
+
+        private static void Jungle()
+        {
+            var target = CF.GetNearest(player.Position);
+            bool hasFlash = player.Spellbook.CanUseSpell(player.GetSpellSlot("SummonerFlash")) == SpellState.Ready;
+            bool smiteReady = ObjectManager.Player.Spellbook.CanUseSpell(smiteSlot) == SpellState.Ready;
+            if (target != null)
+            {
+                CF.setSmiteSlot();
+                double smitedamage = CF.smiteDamage();
+                if (target.CountEnemysInRange(smite.Range)>0)
+                {
+                    if (config.Item("useRJ").GetValue<bool>() && config.Item("useFlashJ").GetValue<bool>() && R.IsReady() && hasFlash && 1000+player.FlatMagicDamageMod >= target.Health &&  player.GetSpell(SpellSlot.R).ManaCost <= player.Mana &&
+                        player.Distance(target.Position) > 400 && player.Distance(target.Position) <= RFlash.Range &&
+                        !player.Position.Extend(target.Position, 400).IsWall())
+                    {
+                        player.Spellbook.CastSpell(player.GetSpellSlot("SummonerFlash"), player.Position.Extend(target.Position, 400));
+                        //Utility.DelayAction.Add(50, () => R.Cast(target, config.Item("packets").GetValue<bool>()));
+                    }
+                }
+                if (config.Item("useRJ").GetValue<bool>() && R.CanCast(target) && !(config.Item("priorizeSmite").GetValue<bool>() && smiteReady) && player.GetSpell(SpellSlot.R).ManaCost <= player.Mana && 1000 + player.FlatMagicDamageMod >= target.Health)
+                {
+                    R.Cast(target, config.Item("packets").GetValue<bool>());
+                } 
+                
+                if (config.Item("useSmite").GetValue<bool>() && smite.CanCast(target) && smiteReady && player.Distance(target) <= smite.Range && CF.smiteDamage() >= target.Health)
+                {
+                    
+                    CF.CastSmite(target);
+                }
+            }
+        }
+
+        private static void Clear()
+        {
+            var minions = ObjectManager.Get<Obj_AI_Minion>().Where(m => m.IsValidTarget(400)).ToList();
+            if (minions.Count() > 2)
+            {
+                if (Items.HasItem(3077) && Items.CanUseItem(3077))
+                    Items.UseItem(3077);
+                if (Items.HasItem(3074) && Items.CanUseItem(3074))
+                    Items.UseItem(3074);
+            }
+            float perc = (float)config.Item("minmana").GetValue<Slider>().Value / 100f;
+            if (player.Mana < player.MaxMana * perc) return;
+            if (W.IsReady() && player.Spellbook.GetSpell(SpellSlot.W).ManaCost <= player.Mana)
+            {
+                var minionsForW = MinionManager.GetMinions(ObjectManager.Player.ServerPosition, W.Range, MinionTypes.All, MinionTeam.NotAlly);
+                MinionManager.FarmLocation bestPositionW = W.GetLineFarmLocation(minionsForW);
+                if (bestPositionW.Position.IsValid())
+                    if (bestPositionW.MinionsHit >= 2)
+                        W.Cast(bestPositionW.Position, config.Item("packets").GetValue<bool>());
+            }
+            if (Q.IsReady() && player.Spellbook.GetSpell(SpellSlot.Q).ManaCost <= player.Mana)
+            {
+                var minionsForQ =
+                    ObjectManager.Get<Obj_AI_Minion>()
+                        .Where(i => i.IsEnemy && !i.IsDead && player.Distance(i.Position) < Q.Range && CF.countMinionsInrange(i, 170f)>1)
+                        .OrderByDescending(i => CF.countMinionsInrange(i, 170f))
+                        .FirstOrDefault();
+                if (minionsForQ.IsValid)Q.Cast(minionsForQ.Position, config.Item("packets").GetValue<bool>());
+            }
+        }
+
+        private static void Harass()
+        {
+            Obj_AI_Hero target = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Magical);
+            if (config.Item("useqH").GetValue<bool>())
+            {
+                if (target.IsValidTarget(Q.Range) && Q.IsReady()) Q.Cast(target, config.Item("packets").GetValue<bool>());
+            }
+            if (config.Item("useeH").GetValue<bool>())
+            {
+                if (target.IsValidTarget(W.Range) && W.IsReady()) W.Cast(target, config.Item("packets").GetValue<bool>());
+            }
+            if (config.Item("useeH").GetValue<bool>() && !vSpikes && E.GetHitCount() > 0)
+            {
+                E.Cast();
+            }
+        }
+
+        private static void Combo()
+        {
+
+            Obj_AI_Hero target = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Magical);
+            bool hasFlash = player.Spellbook.CanUseSpell(player.GetSpellSlot("SummonerFlash")) == SpellState.Ready;
+            bool hasIgnite = player.Spellbook.CanUseSpell(player.GetSpellSlot("SummonerDot")) == SpellState.Ready;
+            var combodmg = CF.ComboDamage(target);
+            if (combodmg > target.Health && hasIgnite)
+            {
+                player.Spellbook.CastSpell(player.GetSpellSlot("SummonerDot"), target);
+            }
+            CF.UseSpells(target);
+                //VorpalSpikes
+                //rupturelaunch
+                //Silence
+
+            if (hasIgnite)
+            {
+                flashRblock = true;
+            }
+            else
+            {
+                flashRblock = false;
+            }
+            if (config.Item("useq").GetValue<bool>())
+            {
+                if (target.IsValidTarget(Q.Range))
+                {
+                    int qHit = config.Item("qHit", true).GetValue<Slider>().Value;
+                    var hitC = HitChance.High;
+                    switch (qHit)
+                    {
+                        case 1:
+                            hitC = HitChance.Low;
+                            break;
+                        case 2:
+                            hitC = HitChance.Medium;
+                            break;
+                        case 3:
+                            hitC = HitChance.High;
+                            break;
+                        case 4:
+                            hitC = HitChance.VeryHigh;
+                            break;
+                    }
+                    Q.CastIfHitchanceEquals(target, hitC, config.Item("packets").GetValue<bool>());
+                }
+            }
+            if (config.Item("usew").GetValue<bool>())
+            {
+                if (target.IsValidTarget(W.Range) && W.IsReady())
+                {
+                    W.Cast(target, config.Item("packets").GetValue<bool>());
+                }
+            }
+
+            if (config.Item("usee").GetValue<bool>() && !vSpikes && E.GetHitCount() > 0 && (CF.countTurretsInRange(player) < 1 || target.Health < 150))
+            {
+                
+                E.Cast();
+            }
+            if (config.Item("UseFlashC").GetValue<bool>() && !flashRblock && R.IsReady() && hasFlash && !CF.CheckCriticalBuffs(target) && player.GetSpell(SpellSlot.R).ManaCost <= player.Mana && player.Distance(target.Position) >= 400 && player.GetSpellDamage(target, SpellSlot.R) > target.Health && !Q.IsReady() && !W.IsReady() && player.Distance(target.Position) <= RFlash.Range && !player.Position.Extend(target.Position, 400).IsWall())
+            {
+                player.Spellbook.CastSpell(player.GetSpellSlot("SummonerFlash"), player.Position.Extend(target.Position, 400));
+                Utility.DelayAction.Add(50, () => R.Cast(target, true));
+            }
+            if (config.Item("UseFlashC").GetValue<bool>() && !flashRblock && W.IsReady() && hasFlash && !CF.CheckCriticalBuffs(target) && player.GetSpell(SpellSlot.W).ManaCost <= player.Mana && player.Distance(target.Position) > W.Range + 300 && player.GetSpellDamage(target, SpellSlot.W) > target.Health && !Q.IsReady() && !R.IsReady() && player.Distance(target.Position) <= W.Range + 400 && !player.Position.Extend(target.Position, 400).IsWall())
+            {
+                player.Spellbook.CastSpell(player.GetSpellSlot("SummonerFlash"), player.Position.Extend(target.Position, 400));
+                Utility.DelayAction.Add(50, () => W.Cast(target, true));
+            }
+            if (config.Item("user").GetValue<bool>() && player.GetSpellDamage(target, SpellSlot.R)>target.Health)
+            {
+                if (target.IsValidTarget(R.Range) && R.IsReady()) R.Cast(target, true);
+            }
+            
+        }
+        private static void OnPossibleToInterrupt(Obj_AI_Base unit, InterruptableSpell spell)
+        {
+            if (config.Item("useQint").GetValue<bool>())
+            {
+                if (unit.IsValidTarget(Q.Range) && Q.IsReady()) Q.Cast(unit, config.Item("packets").GetValue<bool>());
+            }
+            if (config.Item("useWint").GetValue<bool>())
+            {
+                if (unit.IsValidTarget(W.Range) && W.IsReady()) W.Cast(unit, config.Item("packets").GetValue<bool>());
+            }
+        }
+
+        private static void OnEnemyGapcloser(ActiveGapcloser gapcloser)
+        {
+            if (config.Item("useQgc").GetValue<bool>())
+            {
+                if (gapcloser.Sender.IsValidTarget(Q.Range) && Q.IsReady()) Q.Cast(gapcloser.End, config.Item("packets").GetValue<bool>());
+            }
+            if (config.Item("useWgc").GetValue<bool>())
+            {
+                if (gapcloser.Sender.IsValidTarget(W.Range) && W.IsReady()) W.Cast(gapcloser.End, config.Item("packets").GetValue<bool>());
+            }
+        }
+
+        private static void Game_OnDraw(EventArgs args)
+        {
+            CF.DrawCircle("drawaa", player.AttackRange);
+            CF.DrawCircle("drawqq", Q.Range);
+            CF.DrawCircle("drawww", W.Range);
+            CF.DrawCircle("drawrrflash", RFlash.Range);
+            Utility.HpBarDamageIndicator.DamageToUnit = CF.ComboDamage;
+            Utility.HpBarDamageIndicator.Enabled = config.Item("drawcombo").GetValue<bool>();
+        }
+
+        private static void InitChoGath()
+        {
+            Q = new Spell(SpellSlot.Q, 950);
+            Q.SetSkillshot(Q.Instance.SData.SpellCastTime, Q.Instance.SData.LineWidth, Q.Speed, false, SkillshotType.SkillshotCircle);
+            W = new Spell(SpellSlot.W, 700);
+            W.SetSkillshot(W.Instance.SData.SpellCastTime, W.Instance.SData.LineWidth, W.Speed, false, SkillshotType.SkillshotCone);
+            E = new Spell(SpellSlot.E, 500);
+            E.SetSkillshot(E.Instance.SData.SpellCastTime, E.Instance.SData.LineWidth, E.Speed, false, SkillshotType.SkillshotLine);
+            R = new Spell(SpellSlot.R, 175);
+            RFlash = new Spell(SpellSlot.R, 555);
+        }
+
+        private static void InitMenu()
+        {
+            config = new Menu("Cho'Gath", "ChoGath", true);
+            // Target Selector
+            Menu menuTS = new Menu("Selector", "tselect");
+            TargetSelector.AddToMenu(menuTS);
+            config.AddSubMenu(menuTS);
+
+            // Orbwalker
+            Menu menuOrb = new Menu("Orbwalker", "orbwalker");
+            orbwalker = new Orbwalking.Orbwalker(menuOrb);
+            config.AddSubMenu(menuOrb);
+
+            // Draw settings
+            Menu menuD = new Menu("Drawings ", "dsettings");
+            menuD.AddItem(new MenuItem("drawaa", "Draw AA range")).SetValue(new Circle(false, Color.FromArgb(180, 200, 46, 66)));
+            menuD.AddItem(new MenuItem("drawqq", "Draw Q range")).SetValue(new Circle(false, Color.FromArgb(180, 200, 46, 66)));
+            menuD.AddItem(new MenuItem("drawww", "Draw W range")).SetValue(new Circle(false, Color.FromArgb(180, 200, 46, 66)));
+            menuD.AddItem(new MenuItem("drawee", "Draw E range")).SetValue(new Circle(false, Color.FromArgb(180, 200, 46, 66)));
+            menuD.AddItem(new MenuItem("drawrrflash", "Draw R+flash range")).SetValue(new Circle(true, Color.FromArgb(150, 250, 248, 110)));
+            menuD.AddItem(new MenuItem("drawcombo", "Draw combo damage")).SetValue(true);
+            config.AddSubMenu(menuD);
+            // Combo Settings
+            Menu menuC = new Menu("Combo ", "csettings");
+            menuC.AddItem(new MenuItem("useq", "Use Q")).SetValue(true);
+            menuC.AddItem(new MenuItem("qHit", "Q hitChance", true).SetValue(new Slider(3, 1, 4)));
+            menuC.AddItem(new MenuItem("usew", "Use W")).SetValue(true);
+            menuC.AddItem(new MenuItem("usee", "Use E")).SetValue(true);
+            menuC.AddItem(new MenuItem("user", "Use R")).SetValue(true);
+            menuC.AddItem(new MenuItem("UseFlashC", "Use flash")).SetValue(false);
+            config.AddSubMenu(menuC);
+            // Harass Settings
+            Menu menuH = new Menu("Harass ", "Hsettings");
+            menuH.AddItem(new MenuItem("useqH", "Use Q")).SetValue(true);
+            menuH.AddItem(new MenuItem("usewH", "Use W")).SetValue(true);
+            menuH.AddItem(new MenuItem("useeH", "Use E")).SetValue(true);
+            config.AddSubMenu(menuH);
+            // LaneClear Settings
+            Menu menuLC = new Menu("LaneClear ", "Lcsettings");
+            menuLC.AddItem(new MenuItem("useqLC", "Use Q")).SetValue(true);
+            menuLC.AddItem(new MenuItem("usewLC", "Use W")).SetValue(true);
+            menuLC.AddItem(new MenuItem("useeLC", "Use E")).SetValue(true);
+            menuLC.AddItem(new MenuItem("minmana", "Keep X% mana")).SetValue(new Slider(1, 1, 100));
+            config.AddSubMenu(menuLC);
+            // Jungle Settings
+            Menu menuJ = new Menu("Jungle ", "Jsettings");
+            menuJ.AddItem(new MenuItem("useSmite", "Use Smite")).SetValue(true);
+            menuJ.AddItem(new MenuItem("useRJ", "Use R")).SetValue(true);
+            menuJ.AddItem(new MenuItem("priorizeSmite", "Use smite if possible")).SetValue(false);
+            menuJ.AddItem(new MenuItem("useFlashJ", "Use Flash to steal")).SetValue(true);
+            config.AddSubMenu(menuJ);
+            // Misc Settings
+            Menu menuM = new Menu("Misc ", "Msettings");
+            menuM.AddItem(new MenuItem("useQint", "Use Q interrupt")).SetValue(true);
+            menuM.AddItem(new MenuItem("useQgc", "Use Q on gapclosers")).SetValue(false);
+            menuM.AddItem(new MenuItem("useWint", "Use W interrupt")).SetValue(true);
+            menuM.AddItem(new MenuItem("useWgc", "Use W on gapclosers")).SetValue(false);
+            config.AddSubMenu(menuM);
+            config.AddItem(new MenuItem("packets", "Use Packets")).SetValue(false);
+            config.AddToMainMenu();
+        }
+    }
+}
